f2f4e0ca58dec2170ce6d3be360d54235e163cbd
Paul Kruger <paul@miamiphp.org>
2008-10-26 17:33:49 -0400

    updating crunchbase api and adding twitter

---
+++ cakesocial.local/app/vendors/twitter/twitterTalk.php
@@ -0,0 +1,53 @@
+<?php
+set_time_limit(0);
+include('my_twitter.php');
+
+
+class twitterTalk extends MyTwitter{
+	var $sleeptime = 60;
+	var $twitted = array();
+
+	function startTalking(){
+		while(1){
+			
+			$this->checkTwitter();
+			echo '
+
+------------   Checking Next Batch Of Messages ---------------				
+';
+			sleep($this->sleeptime);
+		}	
+	}
+
+	function checkTwitter(){
+		$twits = $this->FollowingTimeLine();
+		foreach($twits as $twit){
+			if (!in_array($twit['id'], $this->twitted)){
+				//not been twitted
+
+			//	if(ereg($this->getUsername(),$twit['username'])){
+
+					$this->say($twit);
+			//	}		
+				
+				//mark as twitted
+				$this->twitted[] = $twit['id'];
+			} 
+		}
+	}
+
+	function say($twit){
+		$msg = $twit['user']['name'] . ' says '. $twit['text'];
+				echo $msg . '
+
+';
+		@exec('say '. $msg);
+	sleep(4);
+	@exec('clear');
+	}
+}
+
+
+$twitterTalk = new twitterTalk('miamiphp','#duelcore55');
+$twitterTalk->startTalking();
+?>
